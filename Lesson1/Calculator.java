df7302bd84744af18a73db540a575936d3ab38e0
Kristina Glushkova <kristinaglushkova@WTMBP-Kristina-Glushkova.local>
2022-07-19 14:38:34 -0400

    Calculator and Guess

---
+++ Lesson1/Calculator.java
@@ -0,0 +1,48 @@
+public class Calculator {
+    
+    public static void main(String[] args) {
+    
+    int num1 = 4;
+    int num2 = 5;
+    String operation = "%";
+    Calculator.doOperation(operation, num1, num2);
+
+
+
+    }
+
+    public static void doOperation(String operation, int num1, int num2) {
+
+    if(operation.equals("+")) {
+        System.out.printf("%d %s %d = %d\n", num1, operation, num2, num1 + num2);
+    } 
+    if(operation.equals("-")) {
+        System.out.printf("%d %s %d = %d\n", num1, operation, num2, num1 - num2);
+    } 
+    if(operation.equals("*")) {
+        System.out.printf("%d %s %d = %d\n", num1, operation, num2, num1 * num2);
+    } 
+    if(operation.equals("/")) {
+        if(num2 != 0) {
+            System.out.printf("%d %s %d = %d\n", num1, operation, num2, num1/num2);
+        }
+    } 
+    if(operation.equals("^")) {
+        System.out.printf("%d %s %d = %d\n", num1, operation, num2, toPow(num1, num2));
+    }
+    if(operation.equals("%")) {
+        System.out.printf("%d %s %d = %d\n", num1, operation, num2, num1 % num2);
+    } 
+
+    }
+
+    public static int toPow(int num, int power) {
+    int result = 1;
+    while(power > 0) {
+        result = result*num;
+        power--;
+    }
+    return result;
+    }
+
+}
---
+++ Lesson1/GuessNumber.java
@@ -0,0 +1,20 @@
+public class GuessNumber {
+    
+    public static void main(String[] args) {
+
+        int compNumber = 67;
+        int playerNumber = 12;
+
+        do {
+            if(compNumber > playerNumber) { 
+                System.out.printf("число %d больше того, что загадал компьютер\n", playerNumber);
+                playerNumber++;
+            } else {
+                System.out.printf("число %d меньше того, что загадал компьютер\n", playerNumber);
+                playerNumber--;
+            }
+        }
+        while (compNumber != playerNumber);
+        System.out.println("Вы победили!");
+    }
+}
