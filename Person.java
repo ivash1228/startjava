c261013e37908aaf51df2e8aed92732e3392770c
Kristina Glushkova <kristinaglushkova@WTMBP-Kristina-Glushkova.local>
2022-07-19 15:30:18 -0400

    Person class

---
+++ Person.java
@@ -0,0 +1,29 @@
+public class Person {
+    
+String sex = "Male";
+String name = "Joe";
+int weight = 90;
+int age = 27;
+
+
+public void walk() {
+    System.out.println("I can walk");
+}
+
+public void seat() {
+    System.out.println("I can seat");
+}
+
+public void run() {
+    System.out.println("I can run");
+}
+
+public void talk() {
+    System.out.println("I can talk");
+    }
+
+public void learnJava() {
+    System.out.println("I can learn Java");
+}
+
+}
