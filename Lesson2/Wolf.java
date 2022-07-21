7d106b391fc4c841afc964c53926f278cdf41136
Kristina Glushkova <kristinaglushkova@WTMBP-Kristina-Glushkova.local>
2022-07-21 13:12:20 -0400

    Wolf and WolfTest

---
+++ Lesson2/Wolf.java
@@ -0,0 +1,29 @@
+public class Wolf {
+
+    String sex;
+    String name;
+    int weight;
+    int age;
+    String color;
+
+    public void walk() {
+        System.out.println("Wolf is walking");
+    }
+
+    public void sit() {
+        System.out.println("Wolf is sitting");
+    }
+
+    public void run() {
+        System.out.println("Wolf is running");
+    }
+
+    public void howl() {
+        System.out.println("Wolf is howling");
+    }
+
+    public void hunt() {
+        System.out.println("Wolf is hunting");
+    }
+
+}
---
+++ Lesson2/WolfTest.java
@@ -0,0 +1,22 @@
+public class WolfTest {
+
+    public static void main(String[] args) {
+        Wolf wolf = new Wolf();
+        wolf.sex = "Male";
+        wolf.name = "Wolf";
+        wolf.weight = 500;
+        wolf.age = 20;
+        wolf.color = "White";
+        System.out.printf("Wolf's name is %s\n", wolf.name);
+        System.out.printf("Wolf's sex is %s\n", wolf.sex);
+        System.out.printf("Wolf's age is %d\n", wolf.age);
+        System.out.printf("Wolf's weight is %d\n", wolf.weight);
+        System.out.printf("Wolf's color is %s\n", wolf.color);
+        wolf.run();
+        wolf.sit();
+        wolf.run();
+        wolf.howl();
+        wolf.hunt();
+
+    } 
+}
