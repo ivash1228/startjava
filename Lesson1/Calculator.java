e075f1b6484f9544dd377982c662436b0f4a0afa
Kristina Glushkova <kristinaglushkova@WTMBP-Kristina-Glushkova.local>
2022-07-21 11:48:09 -0400

    Calculator changed

--- Lesson1/Calculator.java
+++ Lesson1/Calculator.java
@@ -9,32 +9,32 @@ public class Calculator {
     }
 
     public static void doOperation(String operation, int num1, int num2) {
-        if(operation.equals("+")) {
+        if (operation.equals("+")) {
             System.out.printf("%d %s %d = %d\n", num1, operation, num2, num1 + num2);
         } 
         if(operation.equals("-")) {
             System.out.printf("%d %s %d = %d\n", num1, operation, num2, num1 - num2);
         } 
-        if(operation.equals("*")) {
+        if (operation.equals("*")) {
             System.out.printf("%d %s %d = %d\n", num1, operation, num2, num1 * num2);
         } 
-        if(operation.equals("/")) {
-            if(num2 != 0) {
-                System.out.printf("%d %s %d = %d\n", num1, operation, num2, num1/num2);
+        if (operation.equals("/")) {
+            if (num2 != 0) {
+                System.out.printf("%d %s %d = %d\n", num1, operation, num2, num1 / num2);
             }
         } 
-        if(operation.equals("^")) {
+        if (operation.equals("^")) {
             System.out.printf("%d %s %d = %d\n", num1, operation, num2, toPow(num1, num2));
         }
-        if(operation.equals("%")) {
+        if (operation.equals("%")) {
             System.out.printf("%d %s %d = %d\n", num1, operation, num2, num1 % num2);
         } 
     }
 
     public static int toPow(int num, int power) {
         int result = 1;
-        while(power > 0) {
-            result = result*num;
+        while (power > 0) {
+            result = result * num;
             power--;
         }
         return result;
