3abeff5802786c37afc512daf5801d21f6a00f8c
Kristina Glushkova <kristinaglushkova@WTMBP-Kristina-Glushkova.local>
2022-07-22 18:38:04 -0400

    Updated calculator

--- Lesson1/Calculator.java
+++ Lesson1/Calculator.java
@@ -3,38 +3,35 @@ public class Calculator {
     public static void main(String[] args) {
         int num1 = 4;
         int num2 = 5;
-        String operation = "^";
-        Calculator.doOperation(operation, num1, num2);
+        char operation = '%';
+        calculate(operation, num1, num2);
 
     }
 
-    public static void doOperation(String operation, int num1, int num2) {
-        if (operation.equals("+")) {
-            System.out.printf("%d %s %d = %d\n", num1, operation, num2, num1 + num2);
-        } 
-        if (operation.equals("-")) {
-            System.out.printf("%d %s %d = %d\n", num1, operation, num2, num1 - num2);
-        } 
-        if (operation.equals("*")) {
-            System.out.printf("%d %s %d = %d\n", num1, operation, num2, num1 * num2);
-        } 
-        if (operation.equals("/")) {
+    private static void calculate(char operation, int num1, int num2) {
+        int result = 0;
+        if (operation == '+') {
+            result = num1 + num2;
+        } else if (operation == '-') {
+            result =  num1 - num2;
+        } else if (operation == '*') {
+            result =  num1 * num2;
+        } else if (operation == '/') {
             if (num2 != 0) {
-                System.out.printf("%d %s %d = %d\n", num1, operation, num2, num1 / num2);
+                result =  num1 / num2;
             }
-        } 
-        if (operation.equals("^")) {
-            System.out.printf("%d %s %d = %d\n", num1, operation, num2, toPow(num1, num2));
+        } else if (operation == '^') {
+            result = pow(num1, num2);
+        } else {
+            result =  num1 % num2;
         }
-        if (operation.equals("%")) {
-            System.out.printf("%d %s %d = %d\n", num1, operation, num2, num1 % num2);
-        } 
+        System.out.printf("%d %s %d = %d\n", num1, operation, num2, result);
     }
 
-    public static int toPow(int num, int power) {
+    private static int pow(int num, int power) {
         int result = 1;
         while (power > 0) {
-            result = result * num;
+            result *= num;
             power--;
         }
         return result;
