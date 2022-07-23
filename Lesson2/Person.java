b80375375b4dc27717e785904edd89ed4339e7be
Kristina Glushkova <kristinaglushkova@WTMBP-Kristina-Glushkova.local>
2022-07-23 09:37:42 -0400

    Person.java

--- Lesson2/Person.java
+++ Lesson2/Person.java
@@ -1,29 +1,27 @@
 public class Person {
     
-String sex = "Male";
-String name = "Joe";
-int weight = 90;
-int age = 27;
+    String sex = "Male";
+    String name = "Joe";
+    int weight = 90;
+    int age = 27;
 
-
-public void walk() {
-    System.out.println("I can walk");
-}
-
-public void seat() {
-    System.out.println("I can seat");
-}
-
-public void run() {
-    System.out.println("I can run");
-}
-
-public void talk() {
-    System.out.println("I can talk");
+    public void walk() {
+        System.out.println("I can walk");
     }
 
-public void learnJava() {
-    System.out.println("I can learn Java");
-}
+    public void seat() {
+        System.out.println("I can seat");
+    }
 
+    public void run() {
+        System.out.println("I can run");
+    }
+
+    public void talk() {
+        System.out.println("I can talk");
+    }
+
+    public void learnJava() {
+        System.out.println("I can learn Java");
+    }
 }
