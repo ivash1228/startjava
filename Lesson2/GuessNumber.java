eea8ebdf0e4f6d35e5947beabe7c239d5b03c353
Kristina Glushkova <kristinaglushkova@WTMBP-Kristina-Glushkova.local>
2022-07-26 10:11:09 -0400

    Game fixed

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -13,62 +13,55 @@ public class GuessNumber {
         this.playerTwo = playerTwo;
     }
 
-    public void setSecretNumber() {
-        this.secretNumber = new Random().nextInt(100);
+    public void start(Scanner scanner) {
+        generateSecretNumber();
+        boolean result = false;
+            do {
+                System.out.println(playerOne.getName()+ ", put your number: "); 
+                playerOneSetNumber(scanner.nextInt());
+                if (isGuessed(playerOneGetNumber())) {
+                    result = true;
+                    break;
+                }
+                System.out.println(playerTwo.getName()+ ", put your number: "); 
+                playerTwoSetNumber(scanner.nextInt());
+                if (isGuessed(playerTwoGetNumber())) {
+                    result = true;
+                    break;
+                }
+            } while (!result);
     }
 
-    public void playerOneSetNumber(int number) {
+    private void generateSecretNumber() {
+        secretNumber = new Random().nextInt(100) + 1;
+    }
+
+    private boolean isGuessed(int number) {
+    boolean result = false;
+    if (number == secretNumber) {
+        System.out.println("You won!");
+        result = true;
+    } else if (number < secretNumber) {
+        System.out.println("Your number is less.");
+    } else {
+        System.out.println("Your number is greater.");
+    }
+    return result;
+}
+
+    private void playerOneSetNumber(int number) {
         playerOne.setNumber(number);
     }
 
-    public void playerTwoSetNumber(int number) {
+    private void playerTwoSetNumber(int number) {
         playerTwo.setNumber(number);
     }
 
-    public int playerOneGetNumber() {
+    private int playerOneGetNumber() {
         return playerOne.getNumber();
     }
 
-    public int playerTwoGetNumber() {
+    private int playerTwoGetNumber() {
         return playerTwo.getNumber();
-    }
-
-    public boolean isGuessed(int number) {
-        boolean result = false;
-        if (number == secretNumber) {
-            System.out.println("You won!");
-            result = true;
-        } else if (number < secretNumber) {
-            System.out.println("Your number is less.");
-        } else {
-            System.out.println("Your number is greater.");
-        }
-        return result;
-    }
-
-    public void start(Scanner scanner) {
-        String yesNo = "";
-        do {
-            setSecretNumber();
-            boolean result = false;
-                do {
-                    System.out.println(playerOne.getName()+ ", put your number: "); 
-                    playerOneSetNumber(scanner.nextInt());
-                    if (isGuessed(playerOneGetNumber())) {
-                        result = true;
-                        break;
-                    }
-                    System.out.println(playerTwo.getName()+ ", put your number: "); 
-                    playerTwoSetNumber(scanner.nextInt());
-                    if (isGuessed(playerTwoGetNumber())) {
-                        result = true;
-                        break;
-                    }
-                } while (!result);
-                do {
-                    System.out.println("Do you want to play one more time?");
-                    yesNo = scanner.next();
-                } while (!yesNo.equals("yes") && !yesNo.equals("no"));
-        } while (!yesNo.equals("no"));
     }
 }
--- Lesson2/GuessNumberTest.java
+++ Lesson2/GuessNumberTest.java
@@ -9,6 +9,14 @@ public class GuessNumberTest {
         System.out.println("Second player name:");
         Player playerTwo = new Player(scanner.next());
         GuessNumber game = new GuessNumber(playerOne, playerTwo);
-        game.start(scanner);
+        String answer = "";
+        do {
+            game.start(scanner);
+            do {
+                System.out.println("Do you want to play one more time?");
+                answer = scanner.next();
+            } while (!answer.equals("yes") && !answer.equals("no"));
+
+        } while (!answer.equals("no"));
     }
 }
