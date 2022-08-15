cebc56f4112068ac6d3ffcb6e86b4b14f0361cb0
ivash1228 <ivash1228@gmail.com>
2022-08-15 09:12:29 -0400

    SQL updated

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -1,11 +1,11 @@
 CREATE DATABASE Jaegers;
 
 CREATE TABLE Jaegers (
-	id serial PRIMARY KEY,
-	modelName VARCHAR ( 50 ) UNIQUE NOT NULL,
-	mark  char(6) NOT NULL,
-	height INT2,
-	weight INT2,
+    id serial PRIMARY KEY,
+    modelName VARCHAR (50) UNIQUE NOT NULL,
+    mark char(6) NOT NULL,
+    height INT2,
+    weight INT2,
     status char(10),
     origin char(10),
     launch INT2,
--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -4,7 +4,7 @@ SELECT * FROM jaegers
 WHERE status != 'Destroyed';
 
 SELECT * FROM jaegers
-WHERE mark = 'Mark-1' OR mark = 'Mark-6';
+WHERE mark IN ('Mark-1', 'Mark-6');
 
 SELECT * FROM jaegers
 ORDER BY mark DESC;
@@ -20,9 +20,9 @@ WHERE kaijukill = (SELECT MAX(kaijukill) FROM jaegers);
 
 SELECT AVG(weight) AS Average FROM jaegers;
 
-SELECT modelname, kaijukill + 1 as kaijukill FROM jaegers
-WHERE modelname = (SELECT modelname FROM jaegers WHERE status = 'Active');
+UPDATE jaegers SET kaijukill = kaijukill + 1
+WHERE status = 'Active';
 
---DELETE FROM jaegers WHERE status = 'Destroyed';
+DELETE FROM jaegers WHERE status = 'Destroyed';
 
 --psql kristinaglushkova -h 127.0.0.1 -d jaegers -f /Users/kristinaglushkova/Desktop/StartJava/src/com/startjava/lesson_5/init_db.sql -f /Users/kristinaglushkova/Desktop/StartJava/src/com/startjava/lesson_5/queries.sql
